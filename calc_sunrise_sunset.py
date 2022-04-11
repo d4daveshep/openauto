6cdd50c8aa35c7e15a21ae5998010244f2773eee
d4daveshep <david@daveshep.net.nz>
2022-04-12 07:55:05 +1200

    mimicing get_sunrise_sunset function but using calculation not web

--- calc_sunrise_sunset.py
+++ calc_sunrise_sunset.py
@@ -3,47 +3,39 @@ from pytz import timezone
 from skyfield import almanac
 from skyfield.api import S, E, wgs84, load
 
-# Figure out local midnight.
-zone = timezone('Pacific/Auckland')
+def calc_sunrise_sunset():
+    # Figure out local midnight.
+    zone = timezone('Pacific/Auckland')
 
-now = zone.localize(dt.datetime.now())
-print('now',now)
-midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
-print('midnight',midnight)
-next_midnight = midnight + dt.timedelta(days=1)
-print('next_midnight',next_midnight)
-print()
-
-
-ts = load.timescale()
-t0 = ts.from_datetime(midnight)
-t1 = ts.from_datetime(next_midnight)
-eph = load('de421.bsp')
-bluffton = wgs84.latlon(36.8509 * S, 174.7645 * E)
-
-# f = almanac.risings_and_settings(eph, eph['Sun'], bluffton)
-# t, y = almanac.find_discrete(t0, t1, f)
-#
-# for ti, yi in zip(t, y):
-#     print(str(ti.astimezone(zone))[:16], 'Rise' if yi else 'Set')
-#
-# print()
-
-t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, bluffton))
-print(str(t[0].astimezone(zone))[:16])
-print(str(t[1].astimezone(zone))[:16])
-print()
+    now = zone.localize(dt.datetime.now())
+    # print('now',now)
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    # print('midnight',midnight)
+    next_midnight = midnight + dt.timedelta(days=1)
+    # print('next_midnight',next_midnight)
+    # print()
 
 
-# f = almanac.dark_twilight_day(eph, bluffton)
-# times, events = almanac.find_discrete(t0, t1, f)
-#
-# previous_e = f(t0).item()
-# for t, e in zip(times, events):
-#     tstr = str(t.astimezone(zone))[:16]
-#     if previous_e < e:
-#         print(tstr, ' ', almanac.TWILIGHTS[e], 'starts')
-#     else:
-#         print(tstr, ' ', almanac.TWILIGHTS[previous_e], 'ends')
-#     previous_e = e
-print('Done')
+    ts = load.timescale()
+    t0 = ts.from_datetime(midnight)
+    t1 = ts.from_datetime(next_midnight)
+    eph = load('de421.bsp')
+    bluffton = wgs84.latlon(36.8509 * S, 174.7645 * E)
+
+
+    t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, bluffton))
+    sunrise = str(t[0].astimezone(zone))[11:16]
+    sunset = str(t[1].astimezone(zone))[11:16]
+    # print()
+
+    results = {}
+    results['sunrise'] = sunrise
+    results['sunset'] = sunset
+    return results
+
+
+if __name__ == '__main__':
+    results = calc_sunrise_sunset()
+
+    print(f"Sunrise is at: {results['sunrise']}")
+    print(f"Sunset is at: {results['sunset']}")
